95f585c08222a576c28f4c204f02693bdcb53545
Cameron Eldridge <cameldridge+git@gmail.com>
2017-04-04 17:50:40 -0400

    try to fix things

--- src/navigator.js
+++ src/navigator.js
@@ -25,7 +25,9 @@ function parsePath(loc) {
   for(let i = 0; i < parts.length; i += 2) {
     query[parts[i]] = parts[i + 1];
   }
+  console.log(query);
   setLocation(query);
+  window.setTimeout(() => go(query), 0);
 }
 
 function url() {
