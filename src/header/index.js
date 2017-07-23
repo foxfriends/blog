a03d44fe7cc13ea7a31db3dfd43e9c4dcab64d75
Cameron Eldridge <cameldridge+git@gmail.com>
2017-07-23 10:29:06 -0400

    Update header

--- src/header/index.js
+++ src/header/index.js
@@ -6,8 +6,8 @@ class BlogHeader extends React.Component {
   render() {
     return (
       <header className="page__header">
-        <h1 className="page__header_title"><a href="/">Crabs ate the sky</a></h1>
-        <p className="page__header_subtitle">in which I make code pretty, among other things</p>
+        <h1 className="page__header_title"><a href="/">Codes and things</a></h1>
+        <p className="page__header_subtitle">in which I try to make programming like art</p>
       </header>
     )
   }
